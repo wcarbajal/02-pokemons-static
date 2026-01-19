a0d2d13013b8af21034fe02c799ba4d702c69676
wuilmer carbajal <wcarbajal@outlook.com>
2026-01-19 00:43:39 -0500

    pokemon/name

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,md,mdx,js,jsx,ts,tsx,vue,svelte}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
